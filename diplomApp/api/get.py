38c19f8088fe6f845fd8cc48a6567eb243f3f977
KKINIUSS <kisell22@yandex.ru>
2023-12-03 22:59:46 +0300

    'fix'

--- diplomApp/api/get.py
+++ diplomApp/api/get.py
@@ -1,9 +1,11 @@
 import json
-
+import numpy as np
 from rest_framework import status
 from diplomApp.models import AccelerationData
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from scipy.fft import fft
+
 
 @api_view(['POST'])
 def get_data(request):
@@ -13,12 +15,14 @@ def get_data(request):
     to_date = data['to']
     print(AccelerationData.objects.all())
     out = AccelerationData.objects.filter(time__lte=to_date, time__gte=from_date)
-    print(out)
-    ans = []
-    for i in out:
-        ans.append({
-            'time': i.time,
-            'acceleration': i.acceleration
-        })
+    n = len(out)
 
-    return Response(status=status.HTTP_200_OK, data=ans)
+    acceleration = [i.acceleration for i in out]
+
+    dt = from_date - to_date
+    freq = np.fft.fftfreq(n, dt)
+    fft_values = fft(acceleration)
+    magnitude_spectrum = np.abs(fft_values)
+    print(magnitude_spectrum)
+
+    return Response(status=status.HTTP_200_OK)
