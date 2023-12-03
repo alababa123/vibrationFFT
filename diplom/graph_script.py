92e1c909ba2dca8ebe2acb9e068eb302a290decc
KKINIUSS <kisell22@yandex.ru>
2023-12-03 23:03:07 +0300

    '1234'

--- diplom/graph_script.py
+++ diplom/graph_script.py
@@ -2,7 +2,7 @@ import requests
 import matplotlib.pyplot as plt
 
 def get_data_from_server():
-    url = 'http://localhost:8000/api/data/get/'  # Замените на ваш URL
+    url = 'http://localhost:2000/api/data/get/'  # Замените на ваш URL
     response = requests.post(url, json={
         'from': 0,
         'to': 10000
@@ -18,17 +18,17 @@ def get_data_from_server():
 def plot_graph(data):
     print(data)
 
-    if data:
-        time = [int(entry['time']) for entry in data]
-        acceleration = [entry['acceleration'] for entry in data]
+    # if data:
+    #     time = [int(entry['time']) for entry in data]
+    #     acceleration = [entry['acceleration'] for entry in data]
 
-        plt.plot(time, acceleration)
-        plt.title('Acceleration over Time')
-        plt.xlabel('Time')
-        plt.ylabel('Acceleration')
-        plt.show()
-    else:
-        print("No data to plot.")
+    #     plt.plot(time, acceleration)
+    #     plt.title('Acceleration over Time')
+    #     plt.xlabel('Time')
+    #     plt.ylabel('Acceleration')
+    #     plt.show()
+    # else:
+    #     print("No data to plot.")
 
 if __name__ == "__main__":
     data_from_server = get_data_from_server()
